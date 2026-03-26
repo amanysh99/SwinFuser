5e4ec45eb6b7a5fc5989db7a893bb9ca068869b3
amanysh99 <amanysherif@mans.edu.eg>
2026-03-26 18:30:45 +0200

    Add swinFuser_code folder

---
+++ leaderboard/scripts/datagen.sh
@@ -0,0 +1,44 @@
+export CARLA_ROOT=${1:-/media/amany-sherif/New Volume/transfuser/carla}
+export WORK_DIR=${2:-/media/amany-sherif/New Volume/transfuser}
+
+
+# Check if the number of arguments passed is less than 2
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <CARLA_ROOT_PATH> <WORK_DIR_PATH>"
+    exit 1
+fi
+
+# Assign the arguments to variables with proper handling of spaces
+CARLA_ROOT="$1"
+WORK_DIR="$2"
+
+
+export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
+export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
+export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
+
+export SCENARIOS=${WORK_DIR}/leaderboard/data/training/scenarios/Scenario10/Town10HD_Scenario10.json
+export ROUTES=${WORK_DIR}/leaderboard/data/training/routes/Scenario10/Town10HD_Scenario10.xml
+export REPETITIONS=1
+export CHALLENGE_TRACK_CODENAME=MAP
+export CHECKPOINT_ENDPOINT=${WORK_DIR}/results/Town10HD_Scenario10.json
+export SAVE_PATH=${WORK_DIR}/results/Town10HD_Scenario10
+export TEAM_AGENT=${WORK_DIR}/team_code_autopilot/data_agent.py
+export DEBUG_CHALLENGE=0
+export RESUME=1
+export DATAGEN=1
+
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator_local.py \
+--scenarios=${SCENARIOS}  \
+--routes=${ROUTES} \
+--repetitions=${REPETITIONS} \
+--track=${CHALLENGE_TRACK_CODENAME} \
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--agent=${TEAM_AGENT} \
+--agent-config=${TEAM_CONFIG} \
+--debug=${DEBUG_CHALLENGE} \
+--resume=${RESUME}
